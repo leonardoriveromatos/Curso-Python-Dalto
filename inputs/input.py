1effe150e1b4f180f9a77814596df38266791549
Leonardo J. Rivero Matos <leonardojrmatos@gmail.com>
2024-09-05 15:56:54 -0400

    Trabajando con inputs

---
+++ inputs/input.py
@@ -0,0 +1,4 @@
+#Pedirle un dato al usuario, input siempre devuelve una cadena auque escribas un numero, este lo convierte
+nombre = input("Nombre: ")
+
+print(nombre)
---
+++ inputs/input_nombres.py
@@ -0,0 +1,6 @@
+numero = input("Dame un numero: ")
+
+resultado_int = int(numero) * 2
+
+resultado_float = float(numero) * 2
+
